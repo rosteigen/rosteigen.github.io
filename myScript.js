85789d97fced739984698e1f61aaa7c49ef1b356
Rodrigo Devincenzi <rody_009@hotmail.com>
2022-09-15 17:07:52 -0300

    d

--- myScript.js
+++ myScript.js
@@ -1,8 +1,13 @@
-function whenScroll(){
-    if (document.body.onscroll > 200 || document.documentElement.scrollTop > 200){
+
+
+
+function whenScroll() {
+    if (document.body.onscroll > 200 || document.documentElement.scrollTop > 200) {
         document.getElementById("barra").style.background = "black";
-    } 
+    }
     else {
         document.getElementById("barra").style.background = "none";
     }
 }
+
+gsap.fromTo(document.getElementById("foto-banner"),1, {innerHeight: "0%"}, {innerHeight: "100%"});
