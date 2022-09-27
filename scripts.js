fdb7076123bfea3ec7eeea0a367a2b0cab2c812b
Rodrigo Devincenzi <rody_009@hotmail.com>
2022-09-26 21:52:57 -0300

    asd

--- scripts.js
+++ scripts.js
@@ -2,10 +2,12 @@
 
 
 function whenScroll() {
-    if (document.body.onscroll > 200 || document.documentElement.scrollTop > 200) {
+    if (document.body.onscroll > document.body.scrollHeight) {
         document.getElementById("barra").style.background = "black";
     }
     else {
         document.getElementById("barra").style.background = "none";
     }
+
+
 }
