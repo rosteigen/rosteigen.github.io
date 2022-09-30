7e9630e5cf23bdc1d5aa6e0a9f97c6875bd0bd92
Rodrigo Devincenzi <rody_009@hotmail.com>
2022-09-30 16:06:50 -0300

    d

--- scripts.js
+++ scripts.js
@@ -4,8 +4,9 @@
 function whenScroll() {
 
     var titulos = document.getElementsByClassName("titulos");
-    if (document.documentElement.scrollTop >= 500) {
+    if (document.documentElement.scrollTop >= 350) {
         
+        document.getElementById("barra").style.boxShadow = "0 1px 25px #888888";
         document.getElementById("barra").style.animation = "asd";
         document.getElementById("barra").style.animationDuration = "1s";
         document.getElementById("barra").style.animationFillMode = "forwards"
@@ -17,10 +18,18 @@ function whenScroll() {
     }
     else {
         document.getElementById("barra").style.animation = "none";
-
+        document.getElementById("barra").style.boxShadow = "none";
             for(var i = 0; i < titulos.length; i++){
                 titulos[i].style.color = "white";
         }
+    }
+
+    if (document.documentElement.scrollTop >= 900) {
+        
+        document.getElementById("titulo-exp").style.animation = "slide-left";
+        document.getElementById("titulo-exp").style.animationDuration = "1s";
+        
+            
     }
 
 
